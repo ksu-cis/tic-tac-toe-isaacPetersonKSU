7bcd19f683c3d036373cc052d85aa990d67e9de7
ijp <ijp@F1092-12-2100>
2019-11-11 15:21:19 -0600

    beep boop beep beep

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var turn = "x";
+
+function setTurn() {
+    var turnElement = document.getElementById("turn");
+    turnElement.innerText = 'it is player ${turn}'s turn';
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.innerText) {
+        event.target.innerText = turn;
+        if (turn === "x") {
+            turn = "o";
+        }
+        if (turn === "x") {
+            turn = "x";
+        }
+        setTurn
+    }
+}
